445b1f1621dd16fed1e3396294e15c49d6ee4fe3
Kevin Chang <kevin.chang.14159@gmail.com>
2018-09-04 23:06:12 -0700

    Added explanatory comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,10 @@
+// Load Express module
 const express = require('express');
+// Create new router object
 const router = express.Router();
 
+// GET request handled by router
 router.get('/test', (req, res) => res.json({msg: "Posts works"}));
 
+// Return router object as the result of require call
 module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,34 @@
+// Load Express module
 const express = require('express');
+// Load Mongoose module
 const mongoose = require('mongoose');
 
+// Load users.js module
 const users = require('./routes/api/users');
+// Load profile.js module
 const profile = require('./routes/api/profile');
+// Load posts.js module
 const posts = require('./routes/api/posts');
 
+// Create a new Express instance
 const app = express();
 
 // DB config
 const db = require('./config/keys').mongoURI;
-
 // Connect to MongoDB through mongoose
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected')) // Success
   .catch(err => console.log(err)); // Error
 
+// GET request to home page prints Hello World
 app.get('/', (req, res) => res.send('Hello World!'));
 
-// Use routes
+// Use middleware to handle routes via router objects
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
+// Listen on correct port based on environment variable otherwise default to 5000
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
